1ae4c0dee526e4abf3a85c486b2aadc571cb0441
Keelan Fadden-Hopper <info@keelanfh.com>
2023-01-05 18:06:27 +0000

    Starting to bring in the old URLS for the new site scraper

--- new_site_scraper.py
+++ new_site_scraper.py
@@ -1,11 +1,15 @@
 from urllib.parse import urlparse, urljoin
 import scrapy
+import os
+import csv
+import json
 
 from basescraper import BaseScraper
 from constants import MIGRATION_TEST_DOMAIN, MIGRATION_TEST_BASE_URL, MIGRATION_TEST_MOCK_URL, CONTENTFUL_DOMAIN
 
 class NewSiteScraper(BaseScraper):
     name = "newsite"
+    # TODO make start_urls read from the files
     start_urls = [MIGRATION_TEST_BASE_URL, MIGRATION_TEST_MOCK_URL]
     allowed_domains = [MIGRATION_TEST_DOMAIN, CONTENTFUL_DOMAIN]
     
@@ -15,3 +19,26 @@ class NewSiteScraper(BaseScraper):
     # custom_settings = {
     #     "DEPTH_LIMIT": 1
     # }
+
+
+def get_urls():
+    with open("./output/contentful_redirects.json") as f:
+        redirects = json.load(f)
+
+    redirects = [x for x, _ in redirects.items()]
+
+    
+    oldcrawl_latest = [x for x in os.listdir("./output") if x.startswith("oldcrawl")][-1]
+
+    with open(os.path.join("./output", oldcrawl_latest)) as f:
+        old_urls = [x['url'] for x in csv.DictReader(f)]
+    
+    # TODO add in the base URLs to the start or replace the old base URL for the new one as required
+
+    all_urls = redirects + old_urls
+
+    print(all_urls)
+
+get_urls() 
+
+
