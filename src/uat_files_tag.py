8d57911309c0e9f7283ca07b2933200ef907cf97
Keelan Fadden-Hopper <keelan.faddenhopper@essex.gov.uk>
2023-05-22 09:09:53 +0000

    Add UAT for x-robots-tag on docs

--- src/uat_files_tag.py
+++ src/uat_files_tag.py
@@ -12,10 +12,26 @@ e.pptm
 e.pps
 e.ppsx
 e.ppsm
-t/""".split()
+t/e.txt""".split()
 
 for fn in filenames:
-    r = requests.get(f"https://preprod.essex-gov.nomensa.xyz/{fn}")
+    r = requests.get(f"https://beta.essex.gov.uk/{fn}")
     tag = r.headers["x-robots-tag"]
     print(tag)
     assert tag == "noindex"
+    break
+
+filenames_bad = """foo.js
+crea/bar.js
+test/hello.css
+whatever
+something-diffe/rent""".split()
+
+
+for fn in filenames_bad:
+    r = requests.get(f"https://beta.essex.gov.uk/{fn}")
+    try:
+        tag = r.headers["x-robots-tag"]
+    except Exception as e:
+        assert isinstance(e, KeyError)
+    print(tag)
