005d56e9f865ce98e9138fb5238957aca7192866
Keelan Fadden-Hopper <keelan.faddenhopper@essex.gov.uk>
2023-06-09 14:50:58 +0000

    Download a copy of the redirects raw JSON

--- src/redirects_get_contentful.py
+++ src/redirects_get_contentful.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import csv
+import json
 
 # You need to set CDA_API_KEY as an environment variable
 CDA_API_KEY = os.environ["CDA_API_KEY"]
@@ -8,6 +9,9 @@ CDA_API_KEY = os.environ["CDA_API_KEY"]
 redirects = requests.get(
     f"https://cdn.contentful.com/spaces/knkzaf64jx5x/environments/master/entries/638ONr6e4wyaoOo6w8oISU?access_token={CDA_API_KEY}"
 ).json()
+
+with open("./output/contentful_redirects.json", "w+") as f:
+    json.dump(redirects, f)
 
 redirects = redirects["fields"]["redirects"] | redirects["fields"]["legacyUrls"]
 
