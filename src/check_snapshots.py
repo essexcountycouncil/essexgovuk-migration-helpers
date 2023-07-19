0080ca469d2373e7bb331cd0422c1e83d26fcaee
Keelan Fadden-Hopper <info@keelanfh.com>
2023-07-19 15:20:00 +0000

    Update with new, better script from Contentful

--- src/check_snapshots.py
+++ src/check_snapshots.py
@@ -1,11 +1,27 @@
 import json
+from statistics import mean
+from collections import Counter
 
-for version in range(1, 6):
+with open(f"output/snapshots.json") as f:
+    s = json.load(f)
+    versioned_entries = len(s)
+    print(f"Total versioned entries in export:\t{len(s)}")
+    versioned_ids = s.keys()
+    number_of_each = [len(x['items']) for x in s.values()]
+    max_versions, avg_version, counter = max(number_of_each), mean(
+        number_of_each), Counter(number_of_each)
 
-    with open(f"output/snapshots_20230717v{version}.json") as f:
-        s = json.load(f)
-        print(f"Total versioned entries in export v{version}:\t{len(s)}")
-        versioned_ids = s.keys()
+    print(f"Max number of versions: {max_versions}")
+    print(f"Average number of versions: {avg_version}")
+    print(sorted(counter.items()))
+
+    hundreders = [x['items'][0]['snapshot']['fields']['title']['en-GB']
+                  for x in s.values() if len(x['items']) == 100]
+    print(sorted(hundreders))
+
+    hundreder_ids = [key
+                     for key, val in s.items() if len(val['items']) == 100]
+    print(sorted(hundreder_ids))
 
 
 with open("output/contentful-export-knkzaf64jx5x-master-2023-07-17T09-42-06.json") as f:
@@ -16,10 +32,6 @@ with open("output/contentful-export-knkzaf64jx5x-master-2023-07-17T09-42-06.json
         "publishedVersion") is None)
     print(f"Drafts in Contenful export:\t{drafts}")
 
-    original_ids = [x["sys"]["id"] for x in s["entries"]]
+    original_ids = {x["sys"]["id"] for x in s["entries"]}
 
-with open("output/contentful_entry_ids.json", "w") as f:
-    json.dump(original_ids, f)
-
-
-# print(original_ids.difference(versioned_ids))
+    print(original_ids.difference(versioned_ids))
--- versions_download.js
+++ versions_download.js
@@ -6,42 +6,85 @@ const client = contentful.createClient({
 });
 
 const getData = async () => {
+
   let allEntries = []
+
   let entries = {}
 
+
+
   let skip = 0
+
   let total = 100
+
   while (skip < total) {
+
     entries = await client
+
       .getSpace('knkzaf64jx5x')
+
       .then((space) => space.getEnvironment('master'))
+
       .then((env) => env.getEntries({ skip }))
-      .catch(console.error);
+
+      .catch(console.error)
+
+
 
     allEntries = [...allEntries, ...entries.items]
+
     skip += entries.items.length
+
     total = entries.total
+
   }
 
-  const snapshots = {};
+  const totalLenEntries = allEntries.length
 
-  const calls = allEntries.map(async (item) => {
-    return await item
+  console.log('Total Entries count', totalLenEntries)
+
+
+
+  const snapshots = {}
+
+  for (let index = 0; index < totalLenEntries; index++) {
+
+    console.log('Getting Snapshots Progress: %d%', (((index + 1) / totalLenEntries) * 100).toFixed(2))
+
+    const entry = allEntries[index]
+
+    await entry
+
       .getSnapshots()
-      .then((data) => {
-        snapshots[item.sys.id] = data;
-      })
-      .catch(console.error);
-  });
 
-  await Promise.all(calls);
-  console.log('OK', snapshots);
+      .then((data) => {
+
+        snapshots[entry.sys.id] = data;
+
+      })
+
+      .catch(console.error);
+
+  }
+
+
+
+  console.log('Total snapshots for entries', Object.keys(snapshots).length)
+
+
 
   fs.writeFileSync('output/snapshots.json', JSON.stringify(snapshots), 'utf8', (err) => {
-    console.log(err);
-    if (err) throw err;
-    console.log('complete');
-  });
-};
 
-getData();
+    console.log(err)
+
+    if (err) throw err
+
+    console.log('complete')
+
+  })
+
+}
+
+
+
+getData()
